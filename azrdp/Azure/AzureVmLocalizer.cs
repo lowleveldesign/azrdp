ac93ab82b624181550e1e3f83c6638fd3b310e9e
Sebastian Solnica <ssolnica@gmail.com>
2017-06-22 22:55:25 +0200

    finished localizing a VM in the RG, NDesk.Options added

--- azrdp/Azure/AzureVmLocalizer.cs
+++ azrdp/Azure/AzureVmLocalizer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
@@ -9,12 +11,26 @@ namespace LowLevelDesign.AzureRemoteDesktop.Azure
 {
     sealed class AzureVMLocalizer
     {
+        sealed class AzureVM
+        {
+            public string Name { get; set; }
+
+            public bool IsInScaleSet { get; set; }
+
+            public string ScaleSetName { get; set; }
+
+            public string IPAddress { get; set; }
+
+            public string SubnetId { get; set; }
+        }
+
         private readonly AzureResourceManager resourceManager;
 
         private string subscriptionId;
         private string resourceGroupName;
         private string virtualNetworkId;
         private string subnetId;
+        private string targetIPAddress;
 
         public AzureVMLocalizer(AzureResourceManager resourceManager)
         {
@@ -24,6 +40,39 @@ namespace LowLevelDesign.AzureRemoteDesktop.Azure
         public string SubscriptionId { get { return subscriptionId; } }
 
         public string ResourceGroupName { get { return resourceGroupName; } }
+
+        public string VirtualNetworkId { get { return virtualNetworkId; } }
+
+        public string SubnetId { get { return subnetId; } }
+
+        public string TargetIPAddress { get { return targetIPAddress; } }
+
+        public async Task LocalizeVMAsync(string subscription, string resourceGroupName, string vmIPAddress, CancellationToken cancellationToken)
+        {
+            // subscription
+            await FindSubscriptionAsync(subscription, cancellationToken);
+
+            // resource group - we do not validate the name of the resource group
+            if (!string.IsNullOrEmpty(resourceGroupName)) {
+                this.resourceGroupName = resourceGroupName;
+            } else {
+                await PromptForResourceGroupAsync(cancellationToken);
+            }
+
+            // virtual machine
+            if (!string.IsNullOrEmpty(vmIPAddress)) {
+                targetIPAddress = vmIPAddress;
+                await FindVnetAndSubnet(cancellationToken);
+            } else {
+                await PromptForVirtualMachineAsync(cancellationToken);
+            }
+
+            Debug.Assert(subscriptionId != null);
+            Debug.Assert(resourceGroupName != null);
+            Debug.Assert(subnetId != null);
+            Debug.Assert(virtualNetworkId != null);
+            Debug.Assert(targetIPAddress != null);
+        }
 
         private async Task FindSubscriptionAsync(string subscriptionFromArguments, CancellationToken cancellationToken)
         {
@@ -79,13 +128,8 @@ namespace LowLevelDesign.AzureRemoteDesktop.Azure
             subscriptionId = subscriptions[num - 1].Value<string>("subscriptionId");
         }
 
-        private async Task FindResourceGroupAsync(string resourceGroup, CancellationToken cancellationToken)
+        private async Task PromptForResourceGroupAsync(CancellationToken cancellationToken)
         {
-            if (!string.IsNullOrEmpty(resourceGroupName)) {
-                resourceGroupName = resourceGroup;
-                // we do not validate the name of the resource group
-                return;
-            }
             Debug.Assert(subscriptionId != null);
             var resourceGroups = (await resourceManager.GetAsync($"/subscriptions/{subscriptionId}/resourceGroups",
                 cancellationToken))["value"].ToArray();
@@ -116,13 +160,109 @@ namespace LowLevelDesign.AzureRemoteDesktop.Azure
             Debug.Assert(num > 0 && num <= resourceGroups.Length);
             resourceGroupName = resourceGroups[num - 1].Value<string>("name");
         }
-        private async Task FindVnetAndSubnetByIPAddress(string virtualMachineIPAddressTxt, CancellationToken cancellationToken)
+
+        private string GetVirtualNetworkIdFromSubnetId(string subnetId)
+        {
+            var tokens = subnetId.Split('/');
+            return string.Join("/", tokens, 0, tokens.Length - 2);
+        }
+
+        private async Task PromptForVirtualMachineAsync(CancellationToken cancellationToken)
+        {
+            var foundVirtualMachines = new List<AzureVM>();
+
+            var networkInterfaces = (await resourceManager.GetAsync($"/subscriptions/{subscriptionId}/" +
+                $"resourceGroups/{resourceGroupName}/providers/Microsoft.Network/networkInterfaces",
+                cancellationToken))["value"].ToArray();
+
+            foreach (var networkInterface in networkInterfaces) {
+                if (networkInterface["properties"]["virtualMachine"].HasValues) {
+                    var virtualMachineId = networkInterface["properties"]["virtualMachine"].Value<string>("id");
+                    if (networkInterface["properties"]["ipConfigurations"].HasValues) {
+                        var ipConfigurations = networkInterface["properties"]["ipConfigurations"].ToArray();
+                        foreach (var ipConfiguration in ipConfigurations) {
+                            if (string.Equals(ipConfiguration["properties"].Value<string>("provisioningState"),
+                                "Succeeded", StringComparison.Ordinal)) {
+                                foundVirtualMachines.Add(new AzureVM {
+                                    IsInScaleSet = false,
+                                    Name = Path.GetFileName(virtualMachineId),
+                                    IPAddress = ipConfiguration["properties"].Value<string>("privateIPAddress"),
+                                    SubnetId = ipConfiguration["properties"]["subnet"].Value<string>("id")
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+
+            // VM might be in a scale set
+            var scaleSets = (await resourceManager.GetAsync($"/subscriptions/{subscriptionId}/" +
+                $"resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/VirtualMachineScaleSets?api-version=2017-03-30",
+                cancellationToken))["value"].ToArray();
+            foreach (var scaleSet in scaleSets) {
+                var r = await resourceManager.GetAsync(scaleSet.Value<string>("id") + "/networkInterfaces?api-version=2017-03-30",
+                    cancellationToken);
+                networkInterfaces = (r)["value"].ToArray();
+                foreach (var networkInterface in networkInterfaces) {
+                    if (networkInterface["properties"]["virtualMachine"].HasValues) {
+                        var virtualMachineId = networkInterface["properties"]["virtualMachine"].Value<string>("id");
+                        if (networkInterface["properties"]["ipConfigurations"].HasValues) {
+                            var ipConfigurations = networkInterface["properties"]["ipConfigurations"].ToArray();
+                            foreach (var ipConfiguration in ipConfigurations) {
+                                if (string.Equals(ipConfiguration["properties"].Value<string>("provisioningState"),
+                                    "Succeeded", StringComparison.Ordinal)) {
+                                    foundVirtualMachines.Add(new AzureVM {
+                                        IsInScaleSet = true,
+                                        ScaleSetName = scaleSet.Value<string>("name"),
+                                        Name = Path.GetFileName(virtualMachineId),
+                                        IPAddress = ipConfiguration["properties"].Value<string>("privateIPAddress"),
+                                        SubnetId = ipConfiguration["properties"]["subnet"].Value<string>("id")
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (foundVirtualMachines.Count == 0) {
+                throw new ArgumentException($"No running VMs found in the resource group {resourceGroupName}");
+            }
+
+            int num;
+            if (foundVirtualMachines.Count == 1) {
+                num = 1;
+            } else {
+                Console.WriteLine("---------------------------------------------------");
+                Console.WriteLine("VM IP addresses found in the chosen Resource Group:");
+                Console.WriteLine("---------------------------------------------------");
+                for (int i = 0; i < foundVirtualMachines.Count; i++) {
+                    var vm = foundVirtualMachines[i];
+                    Console.WriteLine("[{0}] {1}, ip: {2}", i + 1, 
+                        vm.IsInScaleSet ? $"{vm.ScaleSetName}/{vm.Name}" : vm.Name, vm.IPAddress);
+                }
+
+                Console.Write("Please choose the Virtual Machine number to connect: ");
+                string response = Console.ReadLine();
+                if (!int.TryParse(response, out num) || num < 1 || num > foundVirtualMachines.Count) {
+                    throw new ArgumentException("Invalid Resource Group number");
+                }
+            }
+
+            Debug.Assert(num > 0 && num <= foundVirtualMachines.Count);
+            subnetId = foundVirtualMachines[num - 1].SubnetId;
+            virtualNetworkId = GetVirtualNetworkIdFromSubnetId(subnetId);
+            targetIPAddress = foundVirtualMachines[num - 1].IPAddress;
+        }
+
+        private async Task FindVnetAndSubnet(CancellationToken cancellationToken)
         {
             Debug.Assert(subscriptionId != null);
             Debug.Assert(resourceGroupName != null);
+            Debug.Assert(targetIPAddress != null);
 
             IPAddress virtualMachineIPAddress;
-            if (!IPAddress.TryParse(virtualMachineIPAddressTxt, out virtualMachineIPAddress)) {
+            if (!IPAddress.TryParse(targetIPAddress, out virtualMachineIPAddress)) {
                 throw new ArgumentException("Invalid format of the IP address");
             }
 
@@ -144,49 +284,6 @@ namespace LowLevelDesign.AzureRemoteDesktop.Azure
             }
             throw new ArgumentException("There is no subnet that contains an IP address " +
                     $"'{virtualMachineIPAddress}' in a resource group '{resourceGroupName}'");
-        }
-
-        private async Task FindVirtualMachineByIPAddress(string virtualMachineIPAddressTxt, CancellationToken cancellationToken)
-        {
-            Debug.Assert(subscriptionId != null);
-            Debug.Assert(resourceGroupName != null);
-
-            if (!string.IsNullOrEmpty(virtualMachineIPAddressTxt)) {
-                await FindVnetAndSubnetByIPAddress(virtualMachineIPAddressTxt, cancellationToken);
-            } else {
-                Console.WriteLine("---------------------------------------------------");
-                Console.WriteLine("VM IP addresses found in the chosen Resource Group:");
-                Console.WriteLine("---------------------------------------------------");
-
-                var networkInterfaces = (await resourceManager.GetAsync($"/subscriptions/{subscriptionId}/" +
-                    $"resourceGroups/{resourceGroupName}/providers/Microsoft.Network/networkInterfaces",
-                    cancellationToken))["value"].ToArray();
-
-                // FIXME: I need to save the VMs in a list for later pick
-                foreach (var networkInterface in networkInterfaces) {
-                    if (networkInterface["properties"]["virtualMachine"].HasValues) {
-                        var virtualMachineId = networkInterface["properties"]["virtualMachine"].Value<string>("id");
-                        if (networkInterface["properties"]["ipConfigurations"].HasValues) {
-                            var ipConfigurations = networkInterface["properties"]["ipConfigurations"].ToArray();
-                            foreach (var ipConfiguration in ipConfigurations) {
-                                if (string.Equals(ipConfiguration["properties"].Value<string>("provisioningState"), 
-                                    "Succeeded", StringComparison.Ordinal)) {
-                                    var privateIpAddress = ipConfiguration["properties"].Value<string>("privateIPAddress");
-                                    Console.WriteLine($"FIXME: {virtualMachineId} : {privateIpAddress}");
-                                }
-                            }
-                        }
-                    }
-
-                }
-            }
-        }
-
-        public async Task LocalizeVMAsync(Options options, CancellationToken cancellationToken)
-        {
-            await FindSubscriptionAsync(options.Subscription, cancellationToken);
-            await FindResourceGroupAsync(options.ResourceGroupName, cancellationToken);
-            await FindVirtualMachineByIPAddress(options.VirtualMachineIPAddress, cancellationToken);
         }
     }
 }
--- azrdp/Program.cs
+++ azrdp/Program.cs
@@ -1,8 +1,8 @@
-﻿using CommandLine;
-using LowLevelDesign.AzureRemoteDesktop.Azure;
+﻿using LowLevelDesign.AzureRemoteDesktop.Azure;
+using NDesk.Options;
 using System;
 using System.Diagnostics;
-using System.Net;
+using System.Reflection;
 using System.Threading;
 using Utilities;
 
@@ -23,12 +23,45 @@ namespace LowLevelDesign.AzureRemoteDesktop
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
         static void DoMain(string[] args)
         {
-            var argParseResult = Parser.Default.ParseArguments<Options>(args);
-            if (argParseResult is NotParsed<Options>) {
+            bool showHelp = false, verbose = false;
+            string subscription = null, resourceGroupName = null, vmIPAddress = null;
+
+            var p = new OptionSet
+            {
+                { "s|subscription", "Subscription name or id, where the VM is located.", v => { subscription = v; } },
+                { "r|resgroup", "Resource Group name or id, where the VM is located.", v => { resourceGroupName = v; } },
+                { "i|Virtual Machine IP address", "Resource Group name or id, where the VM is located.", v => { vmIPAddress = v; } },
+                { "v|verbose", "Verbose output.", v => verbose = v != null },
+                { "h|help", "Show this message and exit", v => showHelp = v != null },
+                { "?", "Show this message and exit", v => showHelp = v != null }
+            };
+
+            try {
+                p.Parse(args);
+            } catch (OptionException ex) {
+                Console.Error.Write("ERROR: invalid argument");
+                Console.Error.WriteLine(ex.Message);
+                Console.Error.WriteLine();
+                showHelp = true;
+            } catch (FormatException) {
+                Console.Error.WriteLine("ERROR: invalid number in one of the constraints");
+                Console.Error.WriteLine();
+                showHelp = true;
+            }
+
+            if (!(resourceGroupName == null && subscription == null && vmIPAddress == null ||
+                resourceGroupName != null && subscription != null && vmIPAddress != null)) {
+                Console.Error.WriteLine("ERROR: invalid parameters. Please either provide subscription, resource group, and IP address, or nothing at all.");
+                Console.Error.WriteLine();
+                showHelp = true;
+            }
+
+            if (showHelp) {
+                ShowHelp(p);
                 return;
             }
-            var options = ((Parsed<Options>)argParseResult).Value;
-            if (options.Verbose) {
+
+            if (verbose) {
                 Logger.Level = SourceLevels.Verbose;
             }
 
@@ -36,8 +69,11 @@ namespace LowLevelDesign.AzureRemoteDesktop
                 var resourceManager = new AzureResourceManager();
                 resourceManager.AuthenticateWithPrompt().Wait();
 
-                var vmLocalizer = new AzureVMLocalizer(resourceManager);
-                vmLocalizer.LocalizeVMAsync(options, appCancellationToken).Wait();
+                var targetVM = new AzureVMLocalizer(resourceManager);
+                targetVM.LocalizeVMAsync(subscription, resourceGroupName, vmIPAddress, appCancellationToken).Wait();
+
+                Logger.Log.TraceEvent(TraceEventType.Verbose, 0, "The target VM IP: {0}, VnetId: {1}, SubnetId: {2}, RG: {3}",
+                    targetVM.TargetIPAddress, targetVM.VirtualNetworkId, targetVM.SubnetId, targetVM.ResourceGroupName);
 
                 // FIXME var azureJumpBox = new AzureJumpBox(azure, options.ResourceGroupName, virtualMachineIPAddress);
                 var openSSHWrapper = new OpenSSHWrapper(SupportFiles.SupportFileDir);
@@ -66,6 +102,19 @@ namespace LowLevelDesign.AzureRemoteDesktop
         public static bool Unpack()
         {
             return SupportFiles.UnpackResourcesIfNeeded();
+        }
+
+        static void ShowHelp(OptionSet p)
+        {
+            Console.WriteLine("azrdp v{0} - create a temporary jump host to a VM",
+                Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            Console.WriteLine("Copyright (C) 2017 Sebastian Solnica (@lowleveldesign)");
+            Console.WriteLine();
+            Console.WriteLine("Usage: azrdp [OPTIONS]");
+            Console.WriteLine();
+            Console.WriteLine("Options:");
+            p.WriteOptionDescriptions(Console.Out);
+            Console.WriteLine();
         }
     }
 }
